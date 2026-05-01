7acb1af9d80cef797936b98ba277b220761eda4e
nadinrahmatika951 <nadinrahmatika951@gmail.com>
2026-05-01 16:10:12 +0700

    fix banget

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="en data-theme="lofi">
 
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chirper - Home</title>
