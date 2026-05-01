9c196c9c90271612bdab1f5a031aaff97fdc09e0
nadinrahmatika951 <nadinrahmatika951@gmail.com>
2026-05-01 16:23:25 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,18 @@
 <x-layout>
     <x-slot:title>
         Welcome
-    </x-slot:title>
+    </x-slot :title>
     <div class="max-w-2xl mx-auto">
         @foreach ($chirps as $chirp)
-            <div class="card bg-base-100 shadow mt-8">
-            <div class="card-body">
-                <div>
-                    <div class="font-semibold">{{ $chirp ['author'] }}</div>
-                    <div class="mt-1">{{  $chirp['message'] }}</div>
-                    <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+            <div class="card shadow-xl mt-8 backdrop-blur-lg bg-[#b8c1ec]/25 rounded-2xl border-2 border-[#121629]">
+                <div class="card-body">
+                    <div>
+                        <div class="font-bold text-[#fffffe] font-['Quicksand']">{{ $chirp['author' ] }}</div>
+                        <div class="mt-1 text-[#b8c1ec] font-['Quicksand']">{{ $chirp['message' ] }}</div>
+                        <div class="mt-2 text-xs text-[#b8c1ec] font-['Quicksand']">{{ $chirp['time' ] }}</div>
+                    </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
 </x-layout>
